8010f1f3df2af6f8113752580803bdb3d5abfaf7
BlazyDude <165290997+BlazyDude@users.noreply.github.com>
2024-05-13 21:09:35 +0300

    Add files via upload

---
+++ hesap.js
@@ -0,0 +1,23 @@
+"use strict";
+const ps = require("prompt-sync")
+const prompt = ps();
+var sonuc
+var s1 = prompt("Lütfen ilk sayınızı giriniz: ")
+var s2 = prompt("Lütfen ikinci sayınızı sayi giriniz: ")
+var islem = prompt("Ben bu sayıları napiyim(ı,ç,ö yerine sırasıyla i,c,o yazınız): ")
+var s1 = parseInt(s1)
+var s2 = parseInt(s2)
+if (islem === "topla") {
+    sonuc = s1 + s2
+} else if (islem === "cikar") {
+    sonuc = s1 - s2
+} else if (islem === "carp") {
+    sonuc = s1 * s2
+} else if (islem === "bol") {
+    sonuc = s1 / s2
+} else {
+    sonuc = "Doğru düzgün işlem giriniz"
+};
+
+
+console.log("Çıkan sonuç " + sonuc)
